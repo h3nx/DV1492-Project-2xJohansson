72e1c95cae38506a9adccffd7f31057c16d4b0a1
H3nx <h3nx@hotmail.se>
2017-10-04 09:59:55 +0200

    Started

--- DV1492_Project/DV1492_Project/HelpFiles/shell.cpp
+++ DV1492_Project/DV1492_Project/HelpFiles/shell.cpp
@@ -18,10 +18,25 @@ std::string help();
 
 /* More functions ... */
 
+std::string format();
+std::string ls();
+std::string create();
+std::string cat();
+std::string createImage();
+std::string restoreImage();
+std::string rm();
+std::string cp();
+std::string append();
+std::string mv();
+std::string mkDir();
+std::string cd();
+std::string pwd();
+
+
 int main(void) {
 
 	std::string userCommand, commandArr[MAXCOMMANDS];
-	std::string user = "user@DV1492";    // Change this if you want another user to be displayed
+	std::string user = "Johansson@DV1492";    // Change this if you want another user to be displayed
 	std::string currentDir = "/";    // current directory, used for output
 
     bool bRun = true;
@@ -40,27 +55,37 @@ int main(void) {
 				bRun = quit();                
                 break;
             case 1: // format
+				std::cout << format() << std::endl;
                 break;
             case 2: // ls
                 std::cout << "Listing directory" << std::endl;
                 break;
             case 3: // create
+				std::cout << create() << std::endl;
                 break;
             case 4: // cat
+				std::cout << cat() << std::endl;
                 break;
             case 5: // createImage
+				std::cout << createImage() << std::endl;
                 break;
             case 6: // restoreImage
+				std::cout << restoreImage() << std::endl;
                 break;
             case 7: // rm
+				std::cout << rm() << std::endl;
                 break;
             case 8: // cp
+				std::cout << cp() << std::endl;
                 break;
             case 9: // append
+				//std::cout << append() << std::endl;
                 break;
             case 10: // mv
+				//std::cout << mv() << std::endl;
                 break;
             case 11: // mkdir
+
                 break;
             case 12: // cd
                 break;
@@ -127,4 +152,75 @@ std::string help() {
     return helpStr;
 }
 
+std::string format()
+{
+	return std::string();
+}
+
+std::string ls()
+{
+	return std::string();
+}
+
+std::string create()
+{
+	return std::string();
+}
+
+std::string cat()
+{
+	return std::string();
+}
+
+std::string createImage()
+{
+	return std::string();
+}
+
+std::string restoreImage()
+{
+	return std::string();
+}
+
+std::string rm()
+{
+	return std::string();
+}
+
+std::string cp()
+{
+	return std::string();
+}
+
+std::string append()
+{
+	return std::string();
+}
+
+std::string mv()
+{
+	return std::string();
+}
+
+std::string mkDir()
+{
+	return std::string();
+}
+
+std::string cd()
+{
+	return std::string();
+}
+
+std::string pwd()
+{
+	return std::string();
+}
+
 /* Insert code for your shell functions and call them from the switch-case */
+
+
+
+
+
+
